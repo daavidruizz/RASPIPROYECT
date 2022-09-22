b86dc64a0fa0495fb6ae420d52b907ba1880d618
David Ruiz Luque <daavidruiz01@outlook.com>
2022-09-22 16:10:56 +0200

    ip puerto

--- main.cpp
+++ main.cpp
@@ -1,1 +1,11 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+int main(int arc, char* argv[]){
+    char *ip = "192.168.1.19";
+    int port = 7777;
+
+    if(arc == 1){
+        ip = argv[0];
+    }
+}
